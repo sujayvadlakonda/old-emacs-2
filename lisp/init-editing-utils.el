509911e6a8d38891c677666306c24f235915a359
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-11-01 15:21:06 -0500

    Try truncating lines

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -29,7 +29,7 @@
  scroll-preserve-screen-position 'always
  set-mark-command-repeat-pop t
  tooltip-delay 1.5
- truncate-lines nil
+ truncate-lines t
  truncate-partial-width-windows nil)
 
 (add-hook 'after-init-hook 'delete-selection-mode)
