f6b9e003d037666f0f032555cfb6e119fc43598b
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-11-02 11:08:13 -0500

    Protect gc values when loading whole file

--- init.el
+++ init.el
@@ -28,7 +28,8 @@
 
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
       (init-gc-cons-threshold (* 128 1024 1024)))
-  (setq gc-cons-threshold init-gc-cons-threshold)
+  (unless after-init-time
+    (setq gc-cons-threshold init-gc-cons-threshold))
   (add-hook 'emacs-startup-hook
             (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
