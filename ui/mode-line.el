bf866fa2a27e8790122ee4d99afc5b771c3c598b
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-11-11 09:30:18 -0600

    REmove mode line icons

--- ui/mode-line.el
+++ ui/mode-line.el
@@ -1,30 +1,4 @@
 ;; -*- lexical-binding: t; -*-
 
-(require-package 'doom-modeline)
-(add-hook 'after-init-hook #'doom-modeline-mode)
-(add-hook 'after-init-hook #'column-number-mode)
-
-;; We display project info in the modeline ourselves
-(setq projectile-dynamic-mode-line nil)
-;; Set these early so they don't trigger variable watchers
-(setq doom-modeline-bar-width 3
-      doom-modeline-github nil
-      doom-modeline-mu4e nil
-      doom-modeline-persp-name nil
-      doom-modeline-minor-modes nil
-      doom-modeline-major-mode-icon nil
-      doom-modeline-buffer-file-name-style 'relative-from-project
-      ;; Only show file encoding if it's non-UTF-8 and different line endings
-      ;; than the current OSes preference
-      doom-modeline-buffer-encoding 'nondefault
-      doom-modeline-default-eol-type
-      (cond (OS-WINDOWS 1)
-            (0)))
-
-;; Do not dim mode line when frame is out of focus
-(with-eval-after-load 'doom-modeline
-  (remove-function after-focus-change-function
-                   #'doom-modeline-focus-change))
-
-;; Using a colon to separate line and column confuzzles with time
-(setq doom-modeline-position-column-line-format '("(%l,%c)"))
+;; Do not show minor modes in mode line
+(setq-default mode-line-format (delq 'mode-line-modes mode-line-format))
